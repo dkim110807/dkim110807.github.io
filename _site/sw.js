223d9ac3426555aeb84a540b59999614dcf24943
dkim110807 <dkim110807@gmail.com>
2023-10-01 01:04:33 +0900

    ignore

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1696089352';
+const cacheName = 'chirpy-1696089854';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
